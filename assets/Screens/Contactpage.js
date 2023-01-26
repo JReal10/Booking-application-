10bfa59c4cd71677e2796275f33e8cfe737c81e3
Jamie <jo20322@essex.ac.uk>
2023-01-26 22:00:08 +0000

    completed the contact page

--- assets/Screens/Contactpage.js
+++ assets/Screens/Contactpage.js
@@ -1,13 +1,41 @@
 import * as React from 'react';
 import { Text,View, StyleSheet,SafeAreaView,StatusBar, Button, ScrollView } from 'react-native';
-import Feather from 'react-native-vector-icons/Feather'
 import colors from '../Colors/colors';
 import Buttons from '../Components/ContactButtons';
+import sendEmail from 'react-native-email';
+import call from 'react-native-phone-call';
+import openMap from 'react-native-open-maps';
 
-Feather.loadFont();
-
-//Main contnt for contact page
+//Main content for contact page
 function ContactPage() {
+
+
+
+  const handleEmail = () => {
+  const to = ['tiaan@email.com'] // string or array of email addresses
+  sendEmail(to, {
+      // Optional additional arguments
+      subject: 'Show how to use',
+      body: 'Some body right here'
+  }).catch(console.error)
+}
+
+const handleCall = () => {
+
+  const args = {
+    number: '9093900003', // String value with the number to call
+    prompt: false, // Optional boolean property. Determines if the user should be prompted prior to the call 
+    skipCanOpen: true // Skip the canOpenURL check
+  }
+
+
+  call(args).catch(console.error)
+  };
+
+  const handleMap =() =>{
+    openMap({ latitude: 35.749668, longitude: 139.804901 });
+  }
+
   return (
     <View style = {styles.container}>
       <SafeAreaView>
@@ -28,7 +56,7 @@ function ContactPage() {
             with any enquiries
           </Text>
           <Buttons
-            title="Email Us" 
+            title="Email Us" onPress = {() => handleEmail()} 
           />
         </View>
         <View style = {styles.Container2}>
@@ -41,6 +69,7 @@ function ContactPage() {
           </Text>
           <Buttons
             title="Call Us" 
+            onPress = {() => handleCall()}
           />
         </View>
         <View style = {styles.Container2}>
@@ -52,7 +81,7 @@ function ContactPage() {
             with any enquiries
           </Text>
           <Buttons
-            title="Our Location" 
+            title="Our Location"  onPress = {() => handleMap()}
           />
         </View>
       </View>
